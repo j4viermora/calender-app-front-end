7b5c84f5baec7843322fbbb485dbe43d4beec28a
j4viermora <javmora04@gmail.com>
2021-02-20 19:20:55 -0400

    deplegando build

---
+++ docs/precache-manifest.8e7be634cac6e3e1ce0fa63dc8daf225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b0b3fb218690eaf276f22e91b609db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "885a839c69b20a93d871",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "ffc78d736ccebff9ab51",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "885a839c69b20a93d871",
+    "url": "/static/js/2.810c072a.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.810c072a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc78d736ccebff9ab51",
+    "url": "/static/js/main.f2e4e2b4.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
